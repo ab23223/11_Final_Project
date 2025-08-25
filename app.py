5762db9292220c5d3b6ae3dfe5118563d632a10e
Ashton Balu <ab23223@students.sacredheart.school.nz>
2025-08-25 22:42:57 +0000

    duplicated the page 3 times and added a FAQ for the myths and truth and also added a summary of climate change on one page

--- app.py
+++ app.py
@@ -1,21 +1,22 @@
 from bottle import route, run, template, view, static_file
 
 
-@route('/static/<filename>')
-def server_static(filename):
-    return static_file(filename, root='./static/')
+# Static file serving
+@route('/static/<filepath:path>')
+def server_static(filepath):
+    return static_file(filepath, root='./static')
 
 @route('/')
 def index():
     return template('index')
 
-@route('/about')
-def about():
-    return template('about')
+@route('/whatisclimatechange')
+def whatisclimatechange():
+    return template('whatisclimatechange')
 
-@route('/contact')
-def contact():
-    return template('contact')
+@route('/climatechangeissues')
+def climatechangeissues():
+    return template('climatechangeissues')
 
 
 run(host='localhost', port=8080, debug=True, reloader=True)
